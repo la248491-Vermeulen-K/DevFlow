e1f843ca5e73cb017f39b1b4eaadda9f4022cfaf
Vermeulen Kylian <la248491@student.helha.be>
2026-08-12 11:43:03 +0200

    feat: ajoute l'entité User et sa migration

---
+++ backend/src/main/java/com/vermeulenkylian/backend/model/User.java
@@ -0,0 +1,61 @@
+package com.vermeulenkylian.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Table(name = "users")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true, nullable=false)
+    private String email;
+    @Column(nullable=false, length = 50)
+    private String name;
+    @Column(nullable=false)
+    private String password;
+    @Column(nullable=false)
+    private LocalDateTime createdAt;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
--- backend/src/main/resources/db/migration/V1__create_project_table.sql
+++ backend/src/main/resources/db/migration/V1__create_project_table.sql
@@ -1,10 +1,6 @@
-CREATE DATABASE IF NOT EXISTS myapp;
-
-USE myapp;
-
 CREATE TABLE project (
                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(255) NOT NULL,
                          description VARCHAR(255),
-                         created_at DATETIME
+                         created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
