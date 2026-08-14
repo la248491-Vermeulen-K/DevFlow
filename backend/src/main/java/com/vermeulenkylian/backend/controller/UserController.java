8a6aa29c9e77c4e10a1c548ce92ff2dffb4d200c
Vermeulen Kylian <la248491@student.helha.be>
2026-08-14 12:51:58 +0200

    feat: implémente l'upload d'avatar avec validation

--- backend/src/main/java/com/vermeulenkylian/backend/controller/UserController.java
+++ backend/src/main/java/com/vermeulenkylian/backend/controller/UserController.java
@@ -3,17 +3,21 @@ package com.vermeulenkylian.backend.controller;
 import com.vermeulenkylian.backend.DTO.UpdateProfileRequestDto;
 import com.vermeulenkylian.backend.DTO.UserProfileDto;
 import com.vermeulenkylian.backend.model.User;
+import com.vermeulenkylian.backend.service.AvatarService;
 import com.vermeulenkylian.backend.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final AvatarService avatarService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AvatarService avatarService) {
         this.userService = userService;
+        this.avatarService = avatarService;
     }
 
     @GetMapping("/me")
@@ -21,7 +25,13 @@ public class UserController {
         return userService.getProfile(user);
     }
     @PutMapping("/me")
+
     public UserProfileDto updateProfile(@AuthenticationPrincipal User user, @RequestBody UpdateProfileRequestDto dto) {
         return userService.updateProfile(user, dto);
+    }
+
+    @PostMapping("/me/avatar")
+    public UserProfileDto uploadAvatar(@AuthenticationPrincipal User user, @RequestParam("file") MultipartFile file) {
+        return avatarService.uploadAvatar(user, file);
     }
 }
---
+++ backend/src/main/java/com/vermeulenkylian/backend/service/AvatarService.java
@@ -0,0 +1,71 @@
+package com.vermeulenkylian.backend.service;
+
+import com.vermeulenkylian.backend.DTO.UserProfileDto;
+import com.vermeulenkylian.backend.model.User;
+import com.vermeulenkylian.backend.repository.UserRepository;
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class AvatarService {
+
+    UserRepository userRepository;
+
+    public AvatarService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Value("${app.upload.dir}")
+    private String uploadDir;
+
+    public UserProfileDto uploadAvatar(User user, MultipartFile file) {
+        Tika tika = new Tika();
+        String detectedType = null;
+        try {
+            detectedType = tika.detect(file.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (!Set.of("image/jpeg", "image/png", "image/webp").contains(detectedType)) {
+            throw new IllegalArgumentException("Invalid image type.");
+        }
+        if(file.getSize() <= 0 || file.getSize() > 2 * 1024 * 1024) {
+            throw new IllegalArgumentException("Invalid file size. File is empty or too large.");
+        }
+        String extension = switch (detectedType) {
+            case "image/jpeg" -> ".jpg";
+            case "image/png" -> ".png";
+            case "image/webp" -> ".webp";
+            default -> throw new IllegalArgumentException("Format non supporté");
+        };
+
+        Path uploadPath = Path.of(uploadDir);
+        try {
+            Files.createDirectories(uploadPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create upload directory", e);
+        }
+
+        String filename = UUID.randomUUID() + extension;
+
+        try {
+            file.transferTo(uploadPath.resolve(filename));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not transfer file", e);
+        }
+
+        String avatarUrl = "/uploads/avatars/" + filename;
+        user.setAvatarUrl(avatarUrl);
+        userRepository.save(user);
+        return new UserProfileDto(user.getId(), user.getName(), user.getEmail(), user.getBio(), user.getAvatarUrl(), user.getCreatedAt());
+    }
+}
